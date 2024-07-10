e927dcc606ca9378f45da6718dd6ecbe842c489f
ASEEL ALOMIRI <aseelsalomiri@gmail.com>
2024-07-10 20:11:29 +0300

    Added css and js and img folder

--- pricing/views.py
+++ pricing/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Service
 
 def index(request):
-    return render(request, 'pricing/index.html')
+    services = Service.objects.all()
+    return render(request, 'pricing/index.html', {'services': services})
