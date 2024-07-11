95aa62b7f39cf0d34b24c36694ddb67881769299
ASEEL ALOMIRI <aseelsalomiri@gmail.com>
2024-07-11 19:30:50 +0300

    added files

--- pricing/views.py
+++ pricing/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from .models import Service
+from .models import Offer
+from django.db.models import Sum
+
 
 def index(request):
-    services = Service.objects.all()
-    return render(request, 'pricing/index.html', {'services': services})
+    try:
+        offer = Offer.objects.get(pk=1)  # Replace with your logic to fetch the offer
+        total_price = offer.offerdetail_set.aggregate(total=Sum('total_price'))['total']
+    except Offer.DoesNotExist:
+        offer = None
+        total_price = 0
+
+    context = {
+        'offer': offer,
+        'total_price': total_price,
+    }
+    return render(request, 'pricing/offer_detail.html', context)
