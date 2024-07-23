c0624e29e3c99697cf4f8dc6d03f922fd3f5cc4b
ASEEL ALOMIRI <aseelsalomiri@gmail.com>
2024-07-23 12:42:33 +0300

    added css

--- price_calculator/settings.py
+++ price_calculator/settings.py
@@ -39,7 +39,7 @@ ROOT_URLCONF = 'price_calculator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'pricing/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
